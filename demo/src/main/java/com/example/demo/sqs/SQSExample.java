dec37e3d324600f1c2fd08b25c3f6aeb4fb90baa
Jinhae Kim <cabman21@naver.com>
2024-03-18 08:35:35 +0900

    add sqs

---
+++ demo/src/main/java/com/example/demo/sqs/SQSExample.java
@@ -0,0 +1,206 @@
+//Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//SPDX-License-Identifier: Apache-2.0
+
+package com.example.demo.sqs;
+
+//snippet-start:[sqs.java2.sqs_example.main]
+//snippet-start:[sqs.java2.sqs_example.import]
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ChangeMessageVisibilityRequest;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
+import software.amazon.awssdk.services.sqs.model.ListQueuesRequest;
+import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SqsException;
+import java.util.List;
+//snippet-end:[sqs.java2.sqs_example.import]
+
+/**
+* Before running this Java V2 code example, set up your development
+* environment, including your credentials.
+*
+* For more information, see the following documentation topic:
+*
+* https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+*/
+public class SQSExample {
+ public static void main(String[] args) {
+     String queueName = "queue" + System.currentTimeMillis();
+     SqsClient sqsClient = SqsClient.builder()
+             .region(Region.AP_NORTHEAST_2)
+             .build();
+
+     // Perform various tasks on the Amazon SQS queue.
+     String queueUrl = createQueue(sqsClient, queueName);
+     listQueues(sqsClient);
+     listQueuesFilter(sqsClient, queueUrl);
+     List<Message> messages = receiveMessages(sqsClient, queueUrl);
+     sendBatchMessages(sqsClient, queueUrl);
+     changeMessages(sqsClient, queueUrl, messages);
+     deleteMessages(sqsClient, queueUrl, messages);
+     sqsClient.close();
+ }
+
+ public static String createQueue(SqsClient sqsClient, String queueName) {
+     try {
+         System.out.println("\nCreate Queue");
+         // snippet-start:[sqs.java2.sqs_example.create_queue]
+
+         CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
+                 .queueName(queueName)
+                 .build();
+
+         sqsClient.createQueue(createQueueRequest);
+         // snippet-end:[sqs.java2.sqs_example.create_queue]
+
+         System.out.println("\nGet queue url");
+
+         // snippet-start:[sqs.java2.sqs_example.get_queue]
+         GetQueueUrlResponse getQueueUrlResponse = sqsClient
+                 .getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
+         return getQueueUrlResponse.queueUrl();
+         // snippet-end:[sqs.java2.sqs_example.get_queue]
+
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+     return "";
+ }
+
+ public static void listQueues(SqsClient sqsClient) {
+
+     System.out.println("\nList Queues");
+     // snippet-start:[sqs.java2.sqs_example.list_queues]
+     String prefix = "que";
+
+     try {
+         ListQueuesRequest listQueuesRequest = ListQueuesRequest.builder().queueNamePrefix(prefix).build();
+         ListQueuesResponse listQueuesResponse = sqsClient.listQueues(listQueuesRequest);
+         for (String url : listQueuesResponse.queueUrls()) {
+             System.out.println(url);
+         }
+
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+     // snippet-end:[sqs.java2.sqs_example.list_queues]
+ }
+
+ public static void listQueuesFilter(SqsClient sqsClient, String queueUrl) {
+     // List queues with filters
+     String namePrefix = "queue";
+     ListQueuesRequest filterListRequest = ListQueuesRequest.builder()
+             .queueNamePrefix(namePrefix)
+             .build();
+
+     ListQueuesResponse listQueuesFilteredResponse = sqsClient.listQueues(filterListRequest);
+     System.out.println("Queue URLs with prefix: " + namePrefix);
+     for (String url : listQueuesFilteredResponse.queueUrls()) {
+         System.out.println(url);
+     }
+
+     System.out.println("\nSend message");
+     try {
+         // snippet-start:[sqs.java2.sqs_example.send_message]
+         sqsClient.sendMessage(SendMessageRequest.builder()
+                 .queueUrl(queueUrl)
+                 .messageBody("Hello world!")
+                 .delaySeconds(10)
+                 .build());
+         // snippet-end:[sqs.java2.sqs_example.send_message]
+
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+ }
+
+ public static void sendBatchMessages(SqsClient sqsClient, String queueUrl) {
+
+     System.out.println("\nSend multiple messages");
+     try {
+         // snippet-start:[sqs.java2.sqs_example.send__multiple_messages]
+         SendMessageBatchRequest sendMessageBatchRequest = SendMessageBatchRequest.builder()
+                 .queueUrl(queueUrl)
+                 .entries(SendMessageBatchRequestEntry.builder().id("id1").messageBody("Hello from msg 1").build(),
+                         SendMessageBatchRequestEntry.builder().id("id2").messageBody("msg 2").delaySeconds(10)
+                                 .build())
+                 .build();
+         sqsClient.sendMessageBatch(sendMessageBatchRequest);
+         // snippet-end:[sqs.java2.sqs_example.send__multiple_messages]
+
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+ }
+
+ public static List<Message> receiveMessages(SqsClient sqsClient, String queueUrl) {
+
+     System.out.println("\nReceive messages");
+     // snippet-start:[sqs.java2.sqs_example.retrieve_messages]
+     try {
+         ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                 .queueUrl(queueUrl)
+                 .maxNumberOfMessages(5)
+                 .build();
+         return sqsClient.receiveMessage(receiveMessageRequest).messages();
+
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+     return null;
+     // snippet-end:[sqs.java2.sqs_example.retrieve_messages]
+ }
+
+ public static void changeMessages(SqsClient sqsClient, String queueUrl, List<Message> messages) {
+
+     System.out.println("\nChange Message Visibility");
+     try {
+
+         for (Message message : messages) {
+             ChangeMessageVisibilityRequest req = ChangeMessageVisibilityRequest.builder()
+                     .queueUrl(queueUrl)
+                     .receiptHandle(message.receiptHandle())
+                     .visibilityTimeout(100)
+                     .build();
+             sqsClient.changeMessageVisibility(req);
+         }
+
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+ }
+
+ public static void deleteMessages(SqsClient sqsClient, String queueUrl, List<Message> messages) {
+     System.out.println("\nDelete Messages");
+
+     // snippet-start:[sqs.java2.sqs_example.delete_message]
+     try {
+         for (Message message : messages) {
+             DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
+                     .queueUrl(queueUrl)
+                     .receiptHandle(message.receiptHandle())
+                     .build();
+             sqsClient.deleteMessage(deleteMessageRequest);
+         }
+     } catch (SqsException e) {
+         System.err.println(e.awsErrorDetails().errorMessage());
+         System.exit(1);
+     }
+     // snippet-end:[sqs.java2.sqs_example.delete_message]
+ }
+}
+//snippet-end:[sqs.java2.sqs_example.main]
