6f7f8c550b4838aa40701c41882a6b567a23f195
p.cabman21@kalmate.net <p.cabman21@kalmate.net>
2025-04-11 06:07:40 +0900

    add method

--- demo/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -4,6 +4,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +18,16 @@ public class HelloWorldController {
 	@GetMapping(value = "/helloworld")
 	public String helloWorld() {
 		return "Hello, world!";
+	}
+
+	@GetMapping(value = "/helloworld/v1")
+	public String helloWorldV1(@RequestParam("name") String name) {
+		return "Hello, world! " + name;
+	}
+
+	@GetMapping(value = "/helloworld/v2/{name}")
+	public String helloWorldV2(@PathVariable("name") String name) {
+		return "Hello, world! " + name;
 	}
 
 }
